88bb619e4af6d4e2f32c17db43f1cbba09c5014d
swoopyy <swoopyyy@gmail.com>
2017-03-10 23:42:13 +0300

    add app container

---
+++ src/containers/app.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { Router, Route } from 'react-router'
+import ProductList from '../components/ProductList/ProductList';
+import ProductEditor from '../components/ProductEditor/ProductEditor';
+import ProductAppender from  '../components/ProductAppender/ProductAppender';
+import ProductInfo from '../components/ProductInfo/ProductInfo';
+import {bindActionCreators} from 'redux';
+
+export default class App extends Component {
+  render() {
+    return (
+      <Router history={this.props.history}>
+          <Route path="/" component={ProductList} />
+          <Route path="/edit" component={ProductEditor} />
+          <Route path="/info" component={ProductInfo} />
+          <Route path="/add" component={ProductAppender} />
+      </Router>
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import ReactDOM, { render } from 'react-dom';
+import App from './containers/app';
+import { createStore, applyMiddleware, combineReducers } from 'redux'
+import { browserHistory } from 'react-router'
+import { syncHistoryWithStore, routerReducer } from 'react-router-redux'
+
+import { Provider } from 'react-redux'
+import * as reducers from './reducers';
+import thunk from 'redux-thunk';
 import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
+const reducer =  combineReducers({
+  ...reducers,
+  routing: routerReducer
+});
+const store = createStoreWithMiddleware(reducer);
+const history = syncHistoryWithStore(browserHistory, store)
+
+ReactDOM.render((
+    <Provider store={store}>
+      <App history={history}/>
+    </Provider>
+), document.getElementById('root'));
