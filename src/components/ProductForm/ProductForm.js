34628789a4ee50ab50c0047957275cc38d7da27a
swoopyy <swoopyyy@gmail.com>
2017-03-10 23:41:24 +0300

    add ProductForm mock

---
+++ src/components/ProductForm/ProductForm.js
@@ -0,0 +1,8 @@
+import React, { PropTypes, Component} from 'react';
+import { render } from 'react-dom';
+
+export default class ProductForm extends Component {
+  render() {
+    return <div/>;
+  }
+}
