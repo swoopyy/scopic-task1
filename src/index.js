2cdc82699e6a870f1ad5a2c443cbc08319a8fc28
swoopyy <swoopyyy@gmail.com>
2017-03-13 22:58:15 +0300

    add redux-form reducer

--- src/index.js
+++ src/index.js
@@ -1,22 +1,25 @@
 import React from 'react';
 import ReactDOM, { render } from 'react-dom';
 import App from './containers/app';
-import { createStore, applyMiddleware, combineReducers } from 'redux'
-import { browserHistory } from 'react-router'
-import { syncHistoryWithStore, routerReducer } from 'react-router-redux'
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
 
-import { Provider } from 'react-redux'
+import { browserHistory } from 'react-router';
+import { syncHistoryWithStore, routerReducer } from 'react-router-redux';
+
+import { Provider } from 'react-redux';
 import * as reducers from './reducers';
 import thunk from 'redux-thunk';
 import './index.css';
 
 const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
 const reducer =  combineReducers({
-  ...reducers,
-  routing: routerReducer
+    ...reducers,
+    form: formReducer,
+    routing: routerReducer
 });
 const store = createStoreWithMiddleware(reducer);
-const history = syncHistoryWithStore(browserHistory, store)
+const history = syncHistoryWithStore(browserHistory, store);
 
 ReactDOM.render((
     <Provider store={store}>
