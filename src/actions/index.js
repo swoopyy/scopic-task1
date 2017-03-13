6c46cca654cf894f4c9fe298357b1ae18417128e
swoopyy <swoopyyy@gmail.com>
2017-03-13 23:00:21 +0300

    fix actions and reducers

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,45 +1,46 @@
 import {
-  ADD_PRODUCT,
-  EDIT_PRODUCT,
-  DELETE_PRODUCT,
-  SAVE_PRODUCT,
-  SHOW_PRODUCT,
+    EDIT_PRODUCT,
+    DELETE_PRODUCT,
+    SAVE_PRODUCT,
+    SHOW_PRODUCT,
+    RESET_PRODUCT_TO_SHOW,
+    UPDATE_PRODUCT
 } from '../constants';
-
-export function addProduct(product) {
-  return {
-    type: ADD_PRODUCT,
-    product
-  };
-}
 
 
 export function editProduct(product_id) {
-  return {
-    type: EDIT_PRODUCT,
-    product_id
-  };
+    return {
+        type: EDIT_PRODUCT,
+        product_id
+    };
 }
 
 export function deleteProduct(product_id) {
-  return {
-    type: DELETE_PRODUCT,
-    product_id,
-  };
+    return {
+        type: DELETE_PRODUCT,
+        product_id,
+    };
 }
 
 export function saveProduct(product) {
-  return {
-    type: SAVE_PRODUCT,
-    product
-  };
+    return {
+        type: SAVE_PRODUCT,
+        product
+    };
+}
+
+export function updateProduct(product) {
+    return {
+        type: UPDATE_PRODUCT,
+        product,
+    }
 }
 
 export function showProduct(product_id) {
-  return {
-    type: SHOW_PRODUCT,
-    product_id
-  }
+    return {
+        type: SHOW_PRODUCT,
+        product_id
+    };
 }
 
 export function resetProductToShow() {
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,117 +1,124 @@
 import uuidV1 from 'uuid/v1';
 import {
-  ADD_PRODUCT,
-  EDIT_PRODUCT,
-  DELETE_PRODUCT,
-  UPDATE_PRODUCT,
-  SHOW_PRODUCT,
+    SAVE_PRODUCT,
+    EDIT_PRODUCT,
+    DELETE_PRODUCT,
+    UPDATE_PRODUCT,
+    SHOW_PRODUCT,
+    RESET_PRODUCT_TO_SHOW
 } from '../constants';
 
 var initialState = {
-  products: [
-    {
-      id: uuidV1(),
-      price: 12,
-      name: "Red Delicious Apple",
-      description: "Red Delicious is one of the most famous American apples, and one of the most widely grown apple varieties.",
-      creationDate: new Date(),
-    },
-    {
-      id: uuidV1(),
-      price: 10,
-      name: "Banana",
-      description: "The banana is an edible fruit – botanically a berry– produced by several kinds of large herbaceous flowering plants in the genus Musa.",
-      creationDate: new Date(),
-    },
-    {
-      id: uuidV1(),
-      price: 20,
-      name: "Cherry",
-      decription: "A cherry is the fruit of many plants of the genus Prunus, and is a fleshy drupe (stone fruit).",
-      creationDate: new Date(),
-    },
-    {
-      id: uuidV1(),
-      price: 11,
-      name: "Peach",
-      decription: "The peach (Prunus persica) is a deciduous tree native to the region of Northwest China between the Tarim Basin and the north slopes of the Kunlun Shan mountains, where it was first domesticated and cultivated.",
-      creationDate: new Date(),
-    },
-    {
-      id: uuidV1(),
-      price: 11,
-      name: "Pineapple",
-      decription: "The pineapple (Ananas comosus) is a tropical plant with an edible multiple fruit consisting of coalesced berries, also called pineapples,[2][3] and the most economically significant plant in the Bromeliaceae family.",
-      creationDate: new Date(),
-    },
-  ],
-  productToShow: null,
+    products: [
+        {
+            id: uuidV1(),
+            price: 12,
+            name: "Red Delicious Apple",
+            description: "Red Delicious is one of the most famous American apples, and one of the most widely grown apple varieties.",
+            creationDate: new Date(),
+        },
+        {
+            id: uuidV1(),
+            price: 10,
+            name: "Banana",
+            description: "The banana is an edible fruit – botanically a berry– produced by several kinds of large herbaceous flowering plants in the genus Musa.",
+            creationDate: new Date(),
+        },
+        {
+            id: uuidV1(),
+            price: 20,
+            name: "Cherry",
+            description: "A cherry is the fruit of many plants of the genus Prunus, and is a fleshy drupe (stone fruit).",
+            creationDate: new Date(),
+        },
+        {
+            id: uuidV1(),
+            price: 11,
+            name: "Peach",
+            description: "The peach (Prunus persica) is a deciduous tree native to the region of Northwest China between the Tarim Basin and the north slopes of the Kunlun Shan mountains, where it was first domesticated and cultivated.",
+            creationDate: new Date(),
+        },
+        {
+            id: uuidV1(),
+            price: 11,
+            name: "Pineapple",
+            description: "The pineapple (Ananas comosus) is a tropical plant with an edible multiple fruit consisting of coalesced berries, also called pineapples,[2][3] and the most economically significant plant in the Bromeliaceae family.",
+            creationDate: new Date(),
+        },
+    ],
+    productToShow: null,
 };
 
 function findProductById(id, products) {
-  for (var i = 0; i < products.length; ++i) {
-    if (products[i].id === id) {
-      return products[i];
+    for (var i = 0; i < products.length; ++i) {
+        if (products[i].id === id) {
+            return products[i];
+        }
     }
-  }
 }
 
 function deleteProductById(id, products) {
-  var out_ar = [];
-  for (var i = 0; i < products.length; ++i) {
-    if (products[i].id !== id) {
-      out_ar.push(products[i]);
+    var out = [];
+    for (var i = 0; i < products.length; ++i) {
+        if (products[i].id !== id) {
+            out.push(products[i]);
+        }
     }
-  }
-  return out_ar;
+    return out;
 }
 
-function updateProductById(id, products, newProduct) {
-  for (var i = 0; i < products.length; ++i) {
-    if (products[i].id === id) {
-      products[i] = {
-        id,
-        ...newProduct
-      };
-      return products;
+function updateProduct(product, products) {
+    var out = [];
+    for (var i = 0; i < products.length; ++i) {
+        if (products[i].id === product.id) {
+            out[i] = product;
+        } else {
+            out[i] = products[i];
+        }
     }
-  }
+    return out;
 }
 
-export default function products(state=initialState, action = {}) {
-  switch (action.type) {
-    case ADD_PRODUCT:
-      return {
-        ...state,
-        products: [...state.products, action.product],
-      }
-    case DELETE_PRODUCT:
-      return {
-        ...state,
-        products: deleteProductById(action.product_id, state.products)
-      }
-    case UPDATE_PRODUCT:
-      return {
-        ...state,
-        products: updateProductById(action.product_id, state.products, state.product),
-      }
-    case EDIT_PRODUCT:
-      return {
-        ...state,
-        productToShow: findProductById(action.product_id, state.products),
-      }
-    case SHOW_PRODUCT:
-      return {
-        ...state,
-        productToShow: findProductById(action.product_id, state.products),
-      }
-    default:
-      return state;
-  }
+export default function products(state = initialState, action = {}) {
+    switch (action.type) {
+        case SAVE_PRODUCT:
+            let product = {
+                ...action.product,
+                id: uuidV1(),
+                creationDate: new Date(),
+            }
+            return {
+                ...state,
+                products: [...state.products, product],
+            }
+        case DELETE_PRODUCT:
+            return {
+                ...state,
+                products: deleteProductById(action.product_id, state.products)
+            }
+        case UPDATE_PRODUCT:
+            return {
+                ...state,
+                products: updateProduct(action.product, state.products),
+            }
+        case EDIT_PRODUCT:
+            return {
+                ...state,
+                productToShow: findProductById(action.product_id, state.products),
+            }
+        case SHOW_PRODUCT:
+            return {
+                ...state,
+                productToShow: findProductById(action.product_id, state.products),
+            }
         case RESET_PRODUCT_TO_SHOW:
             console.log("PRODUCT TO SHOW");
             return {
                 ...state,
                 productToShow: null,
+            }
+        default:
+            return state;
+    }
 
 }
